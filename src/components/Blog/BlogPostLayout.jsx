f9f229741e7a86054381f1bd3981a7145d502c98
datahippo93 <cmaine@cea3.com>
2021-09-01 22:23:04 +0800

    Fix footer context issue 2

--- src/components/Blog/BlogPostLayout.jsx
+++ src/components/Blog/BlogPostLayout.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSiteMetadata } from '../../hooks/useSiteMetadata';
 import { Header } from './BlogHeader';
 import Footer from '../Footer/Footer';
+import { PortfolioProvider } from '../../context/context';
+
+import { heroData, aboutData, projectsData, contactData, footerData } from '../../content/data';
 
 const Layout = ({ children }) => {
   const { title, description } = useSiteMetadata();
+
+  const [hero, setHero] = useState({});
+  const [about, setAbout] = useState({});
+  const [projects, setProjects] = useState([]);
+  const [contact, setContact] = useState({});
+  const [footer, setFooter] = useState({});
+
+  useEffect(() => {
+    setHero({ ...heroData });
+    setAbout({ ...aboutData });
+    setProjects([...projectsData]);
+    setContact({ ...contactData });
+    setFooter({ ...footerData });
+  }, []);
+
   return (
     <>
-      <Header siteTitle={title} siteDescription={description} />
-      <section id="blogpost">{children}</section>
-      <Footer />
+      <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+        <Header siteTitle={title} siteDescription={description} />
+        <section id="blogpost">{children}</section>
+        <Footer />
+      </PortfolioProvider>
     </>
   );
 };
